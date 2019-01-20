0bda76c26b1fa1b1fc542d992b4e3ec158221665
Naka <andresnakanishi@gmail.com>
2019-01-19 21:35:17 -0300

    [UPDATE] Deleted Unnecesary Action

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -88,27 +88,6 @@ class UsersController extends AppController
         $this->set(compact('user', 'profiles'));
     }
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
-            $this->Flash->success(__('The user has been deleted.'));
-        } else {
-            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
-
-
     // Configurar el perfil del usuario
 
     public function configData($username = null)
